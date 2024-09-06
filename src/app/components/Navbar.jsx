6b2f4c82c9b8bdc7f54ad2f364e62acbc96274eb
khalid21456 <khalidedaoudi8@gmail.com>
2024-09-06 21:53:31 +0100

    fix hover

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -13,26 +13,26 @@ export default function Navbar() {
           <div className="flex mt-6 ml-10  text-white" style={{fontFamily:"Montserrat"}}>
             <Link
               href="/"
-              className="mr-10 hover:text-orange-400 transition-colors"
+              className="mr-10 hover:text-orange-300 transition-colors"
             >
               Men
             </Link>
-            <Link href="/Product" className="mr-10 hover:text-orange-400 transition-colors">
+            <Link href="/Product" className="mr-10 hover:text-orange-300 transition-colors">
               Womens
             </Link>
-            <Link href="/blog" className="mr-10 hover:text-orange-400 transition-colors">
+            <Link href="/blog" className="mr-10 hover:text-orange-300 transition-colors">
               Beauty
             </Link>
-            <Link href="#" className="mr-10 hover:text-orange-400 transition-colors">
+            <Link href="#" className="mr-10 hover:text-orange-300 transition-colors">
               Sport
             </Link>
-            <Link href="#" className="mr-10 hover:text-orange-400 transition-colors">
+            <Link href="#" className="mr-10 hover:text-orange-300 transition-colors">
               Child
             </Link>
-            <Link href="#" className="mr-10 hover:text-orange-400 transition-colors">
+            <Link href="#" className="mr-10 hover:text-orange-300 transition-colors">
               Special
             </Link>
-            <Link href="#" className="mr-10 hover:text-orange-400 transition-colors">
+            <Link href="#" className="mr-10 hover:text-orange-300 transition-colors">
               Brands
             </Link>
           </div>
