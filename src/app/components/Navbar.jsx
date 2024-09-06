563a94e35af58c3aa0d3d703f48962a2d15ac7b6
khalid21456 <khalidedaoudi8@gmail.com>
2024-09-06 21:50:15 +0100

    finishing hero page

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 import Product from "../Product/page";
 import BookIcon from '@mui/icons-material/Book';
-import "../../../public/logo.png"
 export default function Navbar() {
   return (
     <nav className="w-full h-20 bg-orange-400">
       <div className="flex">
-        <div id="logo" className="h-20 w-60 bg-orange-400">
-          <img alt="logo"  src="../../../public/logo.png"/>
+        <div id="logo" className="h-20 w-60 ml-10 mt-1 bg-orange-400">
+          <Image width={600} height={400} alt="logo" src="/images/logo3.png"/>
         </div>
         <div className="flex justify-between w-[1200px]">
           <div className="flex mt-6 ml-10  text-white" style={{fontFamily:"Montserrat"}}>
