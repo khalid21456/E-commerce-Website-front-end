d9d9eb1f892d3bea43c85f26e19f3b8f5f2f29de
khalid21456 <khalidedaoudi8@gmail.com>
2024-09-06 21:05:02 +0100

    customizing hero page

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Product from "../Product/page";
+import BookIcon from '@mui/icons-material/Book';
+import "../../../public/logo.png"
 export default function Navbar() {
   return (
     <nav className="w-full h-20 bg-orange-400">
       <div className="flex">
-        <div id="logo" className="h-20 w-60 bg-orange-400"></div>
-        <div>
+        <div id="logo" className="h-20 w-60 bg-orange-400">
+          <img alt="logo"  src="../../../public/logo.png"/>
+        </div>
+        <div className="flex justify-between w-[1200px]">
           <div className="flex mt-6 ml-10  text-white" style={{fontFamily:"Montserrat"}}>
             <Link
               href="/"
@@ -31,6 +35,9 @@ export default function Navbar() {
             <Link href="#" className="mr-10 hover:text-orange-400 transition-colors">
               Brands
             </Link>
+          </div>
+          <div className="mt-6">
+            <BookIcon style={{color:"white",cursor:"pointer"}}/>
           </div>
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,32 @@ export default function Home() {
   return (
     <div className="h-[2000px]">
       <div className="h-[500px] bg-orange-400">
-
+        <div className="flex pt-24">
+          <div className="w-[600px] ml-10">
+            <h1
+              style={{ fontFamily: "Poppins" }}
+              className="text-5xl cursor-default font-bold text-white text-left"
+            >
+              Buy Best Products From All Of The World
+            </h1>
+          </div>
+          <div id="heroPage"></div>
+        </div>
+        <div className="ml-10 mt-7">
+          <p className="w-[600px] text-white" style={{ fontFamily: "Roboto" }}>
+            Le Lorem Ipsum est simplement du faux texte employé dans la
+            composition et la mise en page avant impression. Le Lorem Ipsum est
+            le faux texte standard de l'imprimerie depuis les années 1500.
+          </p>
+        </div>
+        <div className="flex justify-around w-[350px] ml-8 mt-9">
+          <button className="bg-white border-2 hover:bg-orange-400 hover:text-white hover:border-2 transition-all text-orange-400 font-bold py-2 px-12 rounded-full">
+            Products
+          </button>
+          <button className="bg-orange-400 hover:bg-orange-300 transition-all text-white border-white border-2 font-bold py-2 px-12 rounded-full">
+            Signup
+          </button>
+        </div>
       </div>
     </div>
   );
